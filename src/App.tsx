245300022c433b4ba3c9b6fe51999f93f541f492
Davi de Morais Farias <davifarias2433@gmail.com>
2025-02-26 16:13:37 -0300

    Config App para utilizar o Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import Layout from './components/Layout'
+import Menu from './components/Menu'
+import { Router } from "./Router"
+
 import './App.css'
 
-function App() {
 
+
+function App() {
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Layout>
+        <Menu /> {/* Menu só deve aparecer quando o usuario estiver logado  */}
+        <Router />
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App
