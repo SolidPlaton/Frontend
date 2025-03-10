3a00434d309dd9a5532e76a10bcf68ddfe232a84
Davi de Morais Farias <davifarias2433@gmail.com>
2025-03-10 10:08:15 -0300

    Add useAuth to Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Login from "./pages/Login";
+import { useAuth } from "./hooks/useAuth";
 
 export function Router() {
+    const {user} = useAuth()
+    const isLogged = !!user
+    const location = useLocation()
 
     return (
-            <Routes location={location} key={location.pathname}>
-                {/* Adicionar paginas da aplicacao aqui!  */}
-                <Route path="/login" element={ <Login /> } />
-            </Routes>
+        <Routes location={location} key={location.pathname}>
+            {
+                !isLogged ?
+                (
+                    <Route path="/login" element={ <Login /> } />
+                )
+                :
+                (
+                    <></>
+                )
+            }
+        </Routes>
     )
 }
