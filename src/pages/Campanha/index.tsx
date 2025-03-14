fa20ea3d2e113f1acc1955b04c312136df63db0d
Davi de Morais Farias <davifarias2433@gmail.com>
2025-03-14 17:22:39 -0300

    Create layout pagina Campanha inicial

--- src/pages/Campanha/index.tsx
+++ src/pages/Campanha/index.tsx
@@ -1,4 +1,10 @@
+import FaseButton from "../../components/FaseButton"
 import earth from "/images/earth-parents-wiki_ver_1-removebg-preview 1.png"
+import meteoro from "/images/fases/meteoro.jpg"
+import tornado from "/images/fases/tornado.jpg"
+import vulcao from "/images/fases/vulcao.jpg"
+import terremoto from "/images/fases/terremoto.jpg"
+import tsunami from "/images/fases/tsunami.jpg"
 
 
 export default function Campanha() {
@@ -9,6 +15,26 @@ export default function Campanha() {
             relative">
             <img draggable="false" src={earth} alt="earth" className="min-w-6xl max-w-6xl
             fixed top-3"/>
+
+            <div className="absolute top-14 left-5/12">
+                <FaseButton img_path={meteoro} />
+            </div>
+
+            <div className="absolute top-52 left-3/12">
+                <FaseButton img_path={tornado} />
+            </div>
+
+            <div className="absolute top-[450px] left-4/12">
+                <FaseButton img_path={vulcao} />
+            </div>
+
+            <div className="absolute top-64 left-[730px]">
+                <FaseButton img_path={terremoto} />
+            </div>
+
+            <div className="absolute top-[500px] left-6/12">
+                <FaseButton img_path={tsunami} />
+            </div>
         </div>
     )
 }
