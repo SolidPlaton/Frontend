558460f9ddc1fb6c7843e7e9298d33d55b1a02ce
Davi de Morais Farias <davifarias2433@gmail.com>
2025-03-14 16:42:19 -0300

    Update images imports

--- src/pages/Campanha/index.tsx
+++ src/pages/Campanha/index.tsx
@@ -1,11 +1,11 @@
-import earth from "../../../public/images/earth-parents-wiki_ver_1-removebg-preview 1.png"
+import earth from "/images/earth-parents-wiki_ver_1-removebg-preview 1.png"
 
 
 export default function Campanha() {
 
     
     return (
-        <div className="bg-[url('../public/images/universo.jpg')] w-screen h-screen overflow-hidden
+        <div className="bg-[url('/images/universo.jpg')] w-screen h-screen overflow-hidden
             relative">
             <img draggable="false" src={earth} alt="earth" className="min-w-6xl max-w-6xl
             fixed top-3"/>
