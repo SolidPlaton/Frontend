b41caa2e9c59e8c9a81024b48f06cf757b64d82a
Davi de Morais Farias <davifarias2433@gmail.com>
2025-03-17 15:01:59 -0300

    Ajustando largura minima e maxima

--- src/components/FaseButton/index.tsx
+++ src/components/FaseButton/index.tsx
@@ -10,7 +10,7 @@ export default function FaseButton({ img_path }:Props) {
 
     return (
         <img draggable="false" src={img_path} alt={img_path} 
-            className="w-24 cursor-pointer border-4 rounded-4xl 
+            className="min-w-[96px] max-w-[96px] cursor-pointer border-4 rounded-4xl 
             hover:border-orange-400 hover:scale-125
             transition duration-150 ease-in-out"/>
     )
