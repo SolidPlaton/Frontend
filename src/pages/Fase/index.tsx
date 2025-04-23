6484fab63585ea8703bac1fa3c89b3316098deb5
Davi de Morais Farias <davifarias2433@gmail.com>
2025-04-23 09:49:50 -0300

    Estilo inicial pagina da Fase

--- src/pages/Fase/index.tsx
+++ src/pages/Fase/index.tsx
@@ -44,10 +44,23 @@ export default function Fase() {
                 </Link>
 
 
-                {img_path && <img draggable="false" src={img_path} alt="Imagem da fase" />}
-                <p>{fase?.id}</p>
-                <p>{fase?.nome}</p>
-                <p>{fase?.assunto}</p>
+                <div className="w-full h-96 flex flex-col justify-between items-center mt-24">
+                    
+                    {img_path && <img draggable="false" src={img_path} alt="Imagem da fase" className="w-40" />}
+                    
+                    <div className="flex flex-col gap-y-5">
+                        <p className="text-white">
+                            melhor pontuação: value pts
+                        </p>
+    
+                        <div className="bg-fuchsia-950 text-white h-12 w-48 rounded-xl cursor-pointer
+                                            flex items-center justify-center
+                                            border-2 border-black hover:border-orange-400 hover:text-orange-400">
+                            Iniciar
+                        </div>
+    
+                    </div>
+                </div>
             </div>
         </Layout>
     )
