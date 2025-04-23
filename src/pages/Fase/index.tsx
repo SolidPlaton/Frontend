0507eca889f1a1e7d8ed1525beed5a5063cbe00e
Davi de Morais Farias <davifarias2433@gmail.com>
2025-04-23 09:18:47 -0300

    Adiciona botao de voltar

--- src/pages/Fase/index.tsx
+++ src/pages/Fase/index.tsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import Layout from "../../components/Layout";
 import { IFase } from "../../interfaces/Fase";
 import { api } from "../../api/axios"
-import { useLocation, useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
+
+import caret_left from "../../../public/images/icons/caret-left.svg"
 
 type FaseParams = {
     id: string;
@@ -30,7 +32,18 @@ export default function Fase() {
 
     return (
         <Layout>
-            <div >
+            <div className="bg-[url('/images/universo.jpg')] w-screen h-screen">
+
+                <Link to='/campanha' className="w-fit flex m-6">
+                    <div className="flex flex-row items-center justify-start w-16">
+                        <img src={caret_left} alt="" className="" />
+                        <p className="text-white">
+                            voltar
+                        </p>
+                    </div>
+                </Link>
+
+
                 {img_path && <img draggable="false" src={img_path} alt="Imagem da fase" />}
                 <p>{fase?.id}</p>
                 <p>{fase?.nome}</p>
