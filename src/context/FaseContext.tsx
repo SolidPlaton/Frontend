2cf1dc7edad617c07df02db1d9591d4193c3356a
Davi de Morais Farias <davifarias2433@gmail.com>
2025-05-04 19:37:17 -0300

    Move questaoAtual e proximaQuestao para contexto

--- src/context/FaseContext.tsx
+++ src/context/FaseContext.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { IQuestao } from "../interfaces/Questao";
 import { createContext, useState } from "react";
 
@@ -6,6 +6,8 @@ import { createContext, useState } from "react";
 interface IFaseContext {
     questoes: IQuestao[]
     resultadoQuestoes: (null | boolean)[]
+    questaoAtual: IQuestao
+    proximaQuestao: () => void
 }
 
 export const FaseContext = createContext({} as IFaseContext)
@@ -18,14 +20,32 @@ type Props = {
 
 export default function FaseContextProvider({children}:Props) {
 
+    const navigate = useNavigate();
     const location = useLocation();
+    
     const questoes: IQuestao[] = location.state?.questoes || [];
+
+    const [indiceQuestao, setIndiceQuestao] = useState(0);
+    const questaoAtual = questoes[indiceQuestao] || null;
 
     const [resultadoQuestoes, setResultadoQuestoes] = useState<(null | boolean)[]>(Array(questoes.length).fill(null));
 
 
+    function proximaQuestao() {
+        setIndiceQuestao(prevIndice => {
+            if (prevIndice < questoes.length - 1) {
+                return prevIndice + 1;
+            } else {
+                navigate('/campanha');
+                return prevIndice;
+            }
+        });
+    }
+    
+
+
     return (
-        <FaseContext.Provider value={{questoes, resultadoQuestoes}}>
+        <FaseContext.Provider value={{questoes, resultadoQuestoes, questaoAtual, proximaQuestao}}>
             {children}
         </FaseContext.Provider>
     )
