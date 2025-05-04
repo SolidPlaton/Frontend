73e3844d8ee390662461c04a48bb44c43979d92b
Davi de Morais Farias <davifarias2433@gmail.com>
2025-05-04 17:40:50 -0300

    New component ProgressoQuestoes

---
+++ src/components/ProgressoQuestoes/index.tsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { FaseContext } from "../../context/FaseContext";
+
+
+
+
+export function ProgressoQuestoes() {
+
+    const { resultadoQuestoes } = useContext(FaseContext);
+
+    return (
+        <div className="flex gap-2 justify-center my-4">
+            {resultadoQuestoes.map((status, index) => (
+                <div
+                    key={index}
+                    className={`w-6 h-6 rounded-full ${
+                        status === null ? "bg-gray-500" : status ? "bg-green-500" : "bg-red-500"
+                    }`}
+                ></div>
+            ))}
+        </div>
+    );
+}
