204b6e88a857ce9b15d3403a5370e6a85825a1e8
Davi de Morais Farias <davifarias2433@gmail.com>
2025-05-14 10:14:23 -0300

    Ajustando espacamento entre elementos pagina Fase

--- src/components/Fase/DisplayQuestoes/BackgroundQuestao.tsx
+++ src/components/Fase/DisplayQuestoes/BackgroundQuestao.tsx
@@ -14,7 +14,7 @@ export default function BackgroundQuestao({ children }:Props) {
             <div className="max-w-2xl min-w-2xl">
                 <div className="w-full flex justify-center">
                     
-                        <div className="w-full flex flex-col items-center pb-16"> 
+                        <div className="w-full flex flex-col items-center pb-16 pt-16"> 
 
                             {children}
                         
--- src/components/Fase/DisplayQuestoes/EstiloQuestao.tsx
+++ src/components/Fase/DisplayQuestoes/EstiloQuestao.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function EstiloQuestao({ children }:Props) {
 
     return (
-        <div className="bg-fuchsia-950 px-8 py-6 mb-24 w-full">
+        <div className="bg-fuchsia-950 px-8 py-6 my-16 w-full">
             { children }
         </div>
     )
