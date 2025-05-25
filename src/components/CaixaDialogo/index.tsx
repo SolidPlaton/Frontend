97c22bdc111c6f7db6d669daa2a8f70392ad099d
Davi de Morais Farias <davifarias2433@gmail.com>
2025-05-25 17:12:59 -0300

    Caixa dialogo inicial

---
+++ src/components/CaixaDialogo/index.tsx
@@ -0,0 +1,39 @@
+
+type Props = {
+    text: string[]
+    onClose: () => void
+}
+
+export default function CaixaDialogo({ text, onClose }:Props) {
+
+    
+    return (
+        <div className="fixed inset-0 flex justify-center items-center z-50">
+            <div className="absolute inset-0" style={{ pointerEvents: "none" }}></div>
+
+
+            {/* Caixa de diálogo */}
+            <div className="relative min-w-160 h-50 z-50 p-px
+                flex flex-row justify-between items-end
+                bg-stone-900 opacity-95 text-neutral-400 border-3 rounded-lg
+                select-none">
+
+                <div className="self-start h-full w-100 pl-4 pt-2">
+
+                    <div className="text-orange-400 text-lg font-inria-sans">
+                        Platão
+                    </div>
+
+                    {text.map((frase, index) => (
+                        <p key={index}>{frase}</p>
+                    ))}
+                </div>
+
+                <div>
+                    <img src="/images/platao/platao-removebg-preview.png" alt="Platão" 
+                        className="w-80" />
+                </div>
+            </div>
+        </div>
+    )
+}   
