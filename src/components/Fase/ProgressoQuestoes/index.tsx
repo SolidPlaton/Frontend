997c351ba46cb7e15fcd50ce5e5f710185f5688e
Davi de Morais Farias <davifarias2433@gmail.com>
2025-06-05 10:53:53 -0300

    Add outline indicativo da questao atual

--- src/components/Fase/ProgressoQuestoes/index.tsx
+++ src/components/Fase/ProgressoQuestoes/index.tsx
@@ -15,9 +15,9 @@ export function ProgressoQuestoes() {
             {respostas.map((status, index) => (
                 <div
                     key={index}
-                    className={`w-6 h-6 rounded-full ${
-                        status === null ? "bg-gray-500" : status.estaCorreta ? "bg-green-500" : "bg-red-500"
-                    }`}
+                    className={`w-6 h-6 rounded-full 
+                        ${ index === faseState.questaoAtualIndex && "outline-4 outline-orange-400" }
+                        ${ status === null ? "bg-gray-500" : status.estaCorreta ? "bg-green-500" : "bg-red-500" }`}
                 ></div>
             ))}
         </div>
