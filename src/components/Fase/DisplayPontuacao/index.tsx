988c1156da4253934c89951ed608a5c5de0d433c
Davi de Morais Farias <davifarias2433@gmail.com>
2025-06-09 02:01:05 -0300

    Display melhor pontuacao

--- src/components/Fase/DisplayPontuacao/index.tsx
+++ src/components/Fase/DisplayPontuacao/index.tsx
@@ -3,13 +3,21 @@ import { FaseContext } from "../../../context/FaseContext";
 
 
 export function DisplayPontuacao() {
-    const { faseState } = useContext(FaseContext);
+    const { faseState, melhorPontuacao } = useContext(FaseContext);
 
     return (
         <div className="text-white select-none fixed right-0 top-0">
             <div className="flex flex-col items-end">
                 <p>pontuação</p>
                 <p className="text-4xl">{ faseState.pontuacaoTotal }</p>
+                {
+                    melhorPontuacao!! && (
+                        <>
+                            <p>melhor pontuação</p>
+                            <p className="text-2xl">{ melhorPontuacao }</p>
+                        </>
+                    )
+                }
             </div>
         </div>
     )
