dd8d8bcb517e85c74ce7577d8f49c4c4a5551e7f
Davi de Morais Farias <davifarias2433@gmail.com>
2025-06-11 21:31:03 -0300

    Update enum

--- src/enums/Dialogos.ts
+++ src/enums/Dialogos.ts
@@ -1,6 +1,12 @@
 enum DialogosEnum {
     introducao = "introducao",
-    game_over = "game_over"
+    game_over = "game_over",
+    Terremoto = "terremoto",
+    Tufão = "tufao",
+    Meteoro = "meteoro",
+    Maremoto = "maremoto",
+    Vulcão = "vulcao"
+
 }
 
 
