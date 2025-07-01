4c342cbed5783d7501785722e49df2065d50f173
Davi de Morais Farias <davifarias2433@gmail.com>
2025-06-30 21:42:19 -0300

    New entities solidos

---
+++ src/entities/solidos.ts
@@ -0,0 +1,27 @@
+import { solido } from "../types/solido"
+
+
+export const dodecaedro:solido = {
+    nome: "Dodecaedro",
+    img_path: "/images/solidos/dodecaedro.png"
+}
+
+export const hexaedro:solido = {
+    nome: "Hexaedro",
+    img_path: "/images/solidos/hexaedro.png"
+}
+
+export const icosaedro:solido = {
+    nome: "Icosaedro",
+    img_path: "/images/solidos/icosaedro.png"
+}
+
+export const tetraedro:solido = {
+    nome: "Tetraedro",
+    img_path: "/images/solidos/tetraedro.png"
+}
+
+export const octaedro:solido = {
+    nome: "Octaedro",
+    img_path: "/images/solidos/octaedro.png"
+}
