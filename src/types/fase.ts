2f642d31f420ef960ba64fe81c5cebfc7d3cfdf9
Davi de Morais Farias <davifarias2433@gmail.com>
2025-06-30 21:38:49 -0300

    New type fase

---
+++ src/types/fase.ts
@@ -0,0 +1,11 @@
+import { solido } from "./solido"
+
+export type fase = {
+    id: number
+    nome: string
+    titulo: string
+    assunto: string
+    curiosidade: string
+    img_path: string
+    solido: solido
+}
