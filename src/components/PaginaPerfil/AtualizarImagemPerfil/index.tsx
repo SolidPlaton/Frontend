e9481bbb15681a0a47346dffb379d78d3ab16862
Davi de Morais Farias <davifarias2433@gmail.com>
2025-07-09 15:16:54 -0300

    Atualiza estilo do modal da imagem de perfil

--- src/components/PaginaPerfil/AtualizarImagemPerfil/index.tsx
+++ src/components/PaginaPerfil/AtualizarImagemPerfil/index.tsx
@@ -43,23 +43,23 @@ export default function AtualizarImagemPerfil() {
             </div>
 
             {mostrarModal && (
-                <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50">
-                    <div className="bg-white p-6 rounded-xl shadow-xl w-[90%] max-w-xl">
-                        <h3 className="text-xl font-bold mb-4 text-black">Escolha uma imagem de perfil:</h3>
-                        <div className="grid grid-cols-3 gap-4">
+                <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-50">
+                    <div className="bg-gray-800 p-6 rounded-xl shadow-xl w-[90%] max-w-xl">
+                        <h3 className="text-xl font-bold mb-4 text-white">Escolha uma imagem de perfil:</h3>
+                        <div className="flex flex-row flex-wrap gap-12 justify-start">
                             {imagensPerfil.map((url, idx) => (
                                 <img
                                     key={idx}
                                     src={url}
                                     alt="Opção de perfil"
-                                    className="w-24 h-24 object-cover cursor-pointer rounded-full hover:ring-4 ring-blue-500"
+                                    className="w-24 h-24 object-cover cursor-pointer rounded-full hover:ring-4 duration-200 ease-out ring-blue-500"
                                     onClick={() => handleSelecionarImagem(url)}
                                 />
                             ))}
                         </div>
                         <button
                             onClick={() => setMostrarModal(false)}
-                            className="mt-6 bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-700"
+                            className="mt-6 text-gray-800 bg-white px-4 py-2 rounded hover:bg-sky-200 cursor-pointer"
                         >
                             Cancelar
                         </button>
