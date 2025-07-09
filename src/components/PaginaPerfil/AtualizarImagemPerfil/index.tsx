385c2d1deb30b7e0946fe2d15c66360b37ff0248
Davi de Morais Farias <davifarias2433@gmail.com>
2025-07-09 15:36:39 -0300

    Coloca Modal em um componente separado

--- src/components/PaginaPerfil/AtualizarImagemPerfil/index.tsx
+++ src/components/PaginaPerfil/AtualizarImagemPerfil/index.tsx
@@ -1,16 +1,9 @@
 import { useState } from "react";
-import { api } from "../../../api/axios";
 import { useAuth } from "../../../hooks/useAuth";
+import { api } from "../../../api/axios";
+import ModalSelecionarImagemPerfil from "../ModalSelecionarImagemPerfil";
 
 
-const imagensPerfil = [
-    "/images/perfil/no-profile-photo-min.jpg",
-    "/images/solidos/dodecaedro.png",
-    "/images/solidos/hexaedro.png",
-    "/images/solidos/icosaedro.png",
-    "/images/solidos/tetraedro.png",
-    "/images/solidos/octaedro.png"
-];
 
 
 export default function AtualizarImagemPerfil() {
@@ -28,11 +21,16 @@ export default function AtualizarImagemPerfil() {
             if (user) {
                 atualizarUser({ ...user, imagemPerfilUrl: novaImagem });
             }
-            setMostrarModal(false);
+            fecharModal()
         } catch (error) {
             console.error("Erro ao atualizar imagem de perfil", error);
         }
     }
+
+    function fecharModal() {
+        setMostrarModal(false)
+    }
+
 
     return (
         <>
@@ -46,29 +44,10 @@ export default function AtualizarImagemPerfil() {
                 </div>
             </div>
 
-            {mostrarModal && (
-                <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-50">
-                    <div className="bg-gray-800 p-6 rounded-xl shadow-xl w-[90%] max-w-xl">
-                        <h3 className="text-xl font-bold mb-4 text-white">Escolha uma imagem de perfil:</h3>
-                        <div className="flex flex-row flex-wrap gap-12 justify-start">
-                            {imagensPerfil.map((url, idx) => (
-                                <img
-                                    key={idx}
-                                    src={url}
-                                    alt="Opção de perfil"
-                                    className="w-24 h-24 object-cover cursor-pointer rounded-full hover:ring-4 duration-200 ease-out ring-blue-500"
-                                    onClick={() => handleSelecionarImagem(url)}
-                                />
-                            ))}
-                        </div>
-                        <button
-                            onClick={() => setMostrarModal(false)}
-                            className="mt-6 text-gray-800 bg-white px-4 py-2 rounded hover:bg-sky-200 cursor-pointer"
-                        >
-                            Cancelar
-                        </button>
-                    </div>
-                </div>
+            {mostrarModal && ( 
+                <ModalSelecionarImagemPerfil 
+                    handleSelecionarImagem={handleSelecionarImagem}
+                    fecharModal={fecharModal} /> 
             )}
         </>
     )
---
+++ src/components/PaginaPerfil/ModalSelecionarImagemPerfil/index.tsx
@@ -0,0 +1,43 @@
+
+const imagensPerfil = [
+    "/images/perfil/no-profile-photo-min.jpg",
+    "/images/solidos/dodecaedro.png",
+    "/images/solidos/hexaedro.png",
+    "/images/solidos/icosaedro.png",
+    "/images/solidos/tetraedro.png",
+    "/images/solidos/octaedro.png"
+];
+
+type Props = {
+    handleSelecionarImagem: (novaImagem: string) => void
+    fecharModal: () => void
+}
+
+export default function ModalSelecionarImagemPerfil({ handleSelecionarImagem, fecharModal }:Props) {
+
+
+    return (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-50">
+            <div className="bg-gray-800 p-6 rounded-xl shadow-xl w-[90%] max-w-xl">
+                <h3 className="text-xl font-bold mb-4 text-white">Escolha uma imagem de perfil:</h3>
+                <div className="flex flex-row flex-wrap gap-12 justify-start">
+                    {imagensPerfil.map((url, idx) => (
+                        <img
+                            key={idx}
+                            src={url}
+                            alt="Opção de perfil"
+                            className="w-24 h-24 object-cover cursor-pointer rounded-full hover:ring-4 duration-200 ease-out ring-blue-500"
+                            onClick={() => handleSelecionarImagem(url)}
+                        />
+                    ))}
+                </div>
+                <button
+                    onClick={() => fecharModal()}
+                    className="mt-6 text-gray-800 bg-white px-4 py-2 rounded hover:bg-sky-200 cursor-pointer"
+                >
+                    Cancelar
+                </button>
+            </div>
+        </div>
+    )
+}
